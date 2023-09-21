8dc2210eee6e2444e9e575c9f9400aa2915e4b2a
miyano <yoshiyahappy03@gmaol.com>
2023-09-21 23:39:32 +0900

    7th commit

--- public/client.js
+++ public/client.js
@@ -41,43 +41,22 @@
 //    e.preventDefault();
 //   viewSample('img2');
 //  }); 
- 
-
-
-//表示サンプル
-let canvas = document.getElementById('canvas');
-let cv = canvas.getContext('2d');
-
-//background color
+ //background color
 //cv.beginPath();
-function bg(){
-	cv.fillStyle = 'ita1.png';
-	cv.fillRect(0, 0, canvas.width, canvas.height);
-	return true
-}
-let isOK = bg()
-console.log(isOK)
 
-//くりぬき文字を入力する度に取得
-let reviewArea = document.getElementById('kurinukimoji');
-reviewArea.addEventListener('input', inputChange);
+// function bg(){
+// 	cv.fillStyle = 'ita1.png';
+// 	cv.fillRect(0, 0, canvas.width, canvas.height);
+// 	return true
+// }
+// let isOK = bg()
+// console.log(isOK)
 
-//くりぬき文字を入力したら動く関数
-function inputChange(){
-	console.log("catch test")
-	let val = reviewArea.value
-	console.log(val)
 
-	//cv.clearRect(0, 0, canvas.width, canvas.height);
-	bg()
 
-	cv.font = '50px Roboto medium';
-	cv.strokeText(val, 100, 100)
 
-	//touka(255,0,0)
-}
 
-//PNG透過画像作成
+//PNG透過画像作成 戻さない
 /* function test(r, g, b) {
 let canvas = document.getElementsByTagName("canvas")[0];
 let context = canvas.getContext("2d");
@@ -95,45 +74,112 @@ let context = canvas.getContext("2d");
 
 
 
-// const canvas = document.getElementById('canvas');
-// const ctx = canvas.getContext('2d');
-const image = new Image();
-value = "ita1.png";
+// const image = new Image();
+// value = "ita1.png";
 
-image.addEventListener("load", ()=>{
-  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+// image.addEventListener("load", ()=>{
+//   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-  ctx.globalCompositeOperation = "destination-in";
-  ctx.fillStyle = 'rgb(0, 0, 0, 1)';
-  ctx.font = "50px 'Meiryo UI'";
-  ctx.fillText("あいうえお", 10 , 50 );
+//   ctx.globalCompositeOperation = "destination-in";
+//   ctx.fillStyle = 'rgb(0, 0, 0, 1)';
+//   ctx.font = "50px 'Meiryo UI'";
+//   ctx.fillText("あいうえお", 10 , 50 );
 
+// });
+
+// //画像ダウンロード
+// function downloadImg () {
+//   const fileName = 'file.png';
+//   const uri = '/path/img/file.png';
+
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', uri);
+//   xhr.responseType = 'blob';
+//   xhr.onloadend = () => {
+//       if (xhr.status !== 200) {
+//           return false;
+//       }
+//       window.navigator.msSaveOrOpenBlob(xhr.response, fileName);
+//   };
+//   xhr.send();
+// }
+
+
+//表示サンプル
+let canvas = document.getElementById('canvas');
+let cv = canvas.getContext('2d');
+
+
+//くりぬき文字を入力する度に取得
+let reviewArea = document.getElementById('kurinukimoji');
+reviewArea.addEventListener('input', inputChange);
+
+//くりぬき文字を入力したら動く関数
+function inputChange(){
+	
+	// cv.font = '50px Roboto medium';
+	// cv.strokeText(val, 100, 100)
+  func1() 
+}
+
+const image1 = new Image();
+const image2 = new Image();
+const image3 = new Image();
+const image4 = new Image();
+image1.src = "/public/ita1.png";
+image2.src = "/public/ita2.png";
+image3.src = "/public/ita3.png";
+image4.src = "/public/ita4.png";
+
+
+image1.addEventListener("load", ()=>{
+  setimage(image1);
+  
 });
+function setimage(image) {
+  // cv.clearRect(0, 0, canvas.width, canvas.height);
+  cv.globalCompositeOperation = "source-over";
+  cv.drawImage(image, 0, 0, canvas.width, canvas.height);
+  
+  cv.globalCompositeOperation = "destination-in";
+  cv.fillStyle = 'rgb(0, 0, 0, 1)';
+  // cv.font = "50px Roboto";
+  cv.fillText(reviewArea.value, 10 , 50 );  
+  
+}
 
-//ラジオボタンを押したらイベント
-function check_form() {
-  var value = document.sample_form.sex;
 
-  for(i=0; i<value.length; i++) {
-    if(value[i].checked) {
-      console.log(value[i].value);
+//ラジオボタンを押したらキャッチ
+function func1() {
+  var fruits = document.getElementsByName("ita");
+  for(var i = 0; i < fruits.length; i++){
+    if(fruits[i].checked) {
+      console.log("選択された値：", fruits[i].value);
+
+      //選択されたものによって画像を変える
+      if (fruits[i].value=="ita1") {
+        
+        setimage(image1);
+      }
+      else if (fruits[i].value=="ita2"){
+        
+        setimage(image2);
+      }
+      else if (fruits[i].value=="ita3"){
+       
+        setimage(image3);
+      }
+      else {
+        
+        setimage(image4);
+      }
+      
+      
     }
   }
 }
+func1();
 
-//画像ダウンロード
-function downloadImg () {
-  const fileName = 'file.png';
-  const uri = '/path/img/file.png';
 
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', uri);
-  xhr.responseType = 'blob';
-  xhr.onloadend = () => {
-      if (xhr.status !== 200) {
-          return false;
-      }
-      window.navigator.msSaveOrOpenBlob(xhr.response, fileName);
-  };
-  xhr.send();
-}
+
+
